3a095a4f002e1031f66a09e5c2e34df0454e5519
judescha <julesdeschamps24@gmail.com>
2025-11-02 06:19:35 +0100

    more test for mutant stack

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,63 +3,80 @@
 int main()
 {
     {
-    std::cout << "Mutant stack: " << std::endl;
-    MutantStack<int> mstack;
+		std::cout << "Mutant stack: " << std::endl;
+		MutantStack<int> mstack;
 
-    mstack.push(5);
-    mstack.push(17);
+		mstack.push(5);
+		mstack.push(17);
 
-    std::cout << mstack.top() << std::endl;
+		std::cout << mstack.top() << std::endl;
 
-    mstack.pop();
+		mstack.pop();
 
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-//[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+	//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 
-    ++it;
-    --it;
-    while (it != ite)
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << std::endl;
+    }
+
+	{
+		std::cout << "Copy test: " << std::endl;
+		MutantStack<int> ms;
+		std::cout << ms.size() << std::endl;
+
+		MutantStack<int> og_ms;
+		og_ms.push(69);
+		og_ms.push(69);
+		og_ms.push(69);
+		og_ms.push(69);
+		og_ms.push(69);
+		std::cout << "og_ms: " << og_ms.size() << std::endl;
+		ms = og_ms;
+		std::cout << "ms after copy: " << ms.size() << std::endl;
+		std::cout << std::endl;
+	}
+
     {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    std::stack<int> s(mstack);
-    std::cout << std::endl;
-    }
+		std::cout << "std::list : " << std::endl;
+		std::list<int> list;
 
-    {
-    std::cout << "std::list : " << std::endl;
-    std::list<int> list;
+		list.push_back(5);
+		list.push_back(17);
 
-    list.push_back(5);
-    list.push_back(17);
+		std::cout << list.back() << std::endl;
 
-    std::cout << list.back() << std::endl;
+		list.pop_back();
 
-    list.pop_back();
+		std::cout << list.size() << std::endl;
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+	//[...]
+		list.push_back(0);
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
 
-    std::cout << list.size() << std::endl;
-    list.push_back(3);
-    list.push_back(5);
-    list.push_back(737);
-//[...]
-    list.push_back(0);
-    std::list<int>::iterator it = list.begin();
-    std::list<int>::iterator ite = list.end();
-
-    ++it;
-    --it;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    }
-    return 0;
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	return 0;
 }
